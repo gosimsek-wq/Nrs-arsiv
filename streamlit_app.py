50ec37686d45f6cac92161cc67e6f2b71bebaf3c
gosimsek-wq <gosimsek@hotmail.com>
2026-02-13 11:18:34 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from oauth2client.service_account 
-import ServiceAccountCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 import hashlib 
 from datetime 
 import datetime
